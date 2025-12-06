b4705d3c506873c07ed603181c6787a92c0c2456
johnarclear-cloud <johnarclear@gmail.com>
2025-12-06 17:03:13 +0000

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,12 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+
+nr = InitNornir(config_file="config.yaml")
+
+def test_connect(task):
+  task.run(task=netmiko_send_command, command_string="show configuration system host-name | display xml")
+  
+result = nr.run(task=test_connect)
+print_result(result)
