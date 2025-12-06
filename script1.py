db57639b1b806b8bc39f8c605e057c1d1b7e6fd0
johnarclear-cloud <johnarclear@gmail.com>
2025-12-06 18:44:35 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -6,7 +6,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
-  task.run(task=netmiko_send_command, command_string="show configuration system host-name | display xml")
+  task.run(task=netmiko_send_command, command_string="show configuration system host-name | display set")
   
 result = nr.run(task=test_connect)
 print_result(result)
