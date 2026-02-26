e8c7b53c23440b0f1132ddc5146faf621c811836
johnarclear-cloud <johnarclear@gmail.com>
2026-02-26 23:27:34 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -6,7 +6,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
-  task.run(task=netmiko_send_command, command_string="show interfaces terse | display json")
+  task.run(task=netmiko_send_command, command_string="show interfaces terse | display xml")
   
 result = nr.run(task=test_connect)
 print_result(result)
